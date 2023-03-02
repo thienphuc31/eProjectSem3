41667c9bdb673aa51a3c311eafdd827ef3fd81a5
thienphuc31 <kirito3175@gmail.com>
2023-03-02 17:42:22 +0700

    Cap nhat moi nhat

--- Areas/Admin/Controllers/EmployeeEventController.cs
+++ Areas/Admin/Controllers/EmployeeEventController.cs
@@ -20,25 +20,60 @@ namespace eProject1.Areas.Admin.Controllers
         }
         public IActionResult Index()
         {
-            var employeeEvents = db.EmployeeEvents.Include(e => e.employees).Include(e => e.events);
-            ViewBag.ChangedEvents = HttpContext.Session.TryGetValue("changed_events", out byte[] value) ? JsonConvert.DeserializeObject<Dictionary<int, bool>>(System.Text.Encoding.UTF8.GetString(value)) : new Dictionary<int, bool>();
-            return View(employeeEvents.ToList());
+            var employeeEvents = db.EmployeeEvents.Include(e => e.employees).Include(e => e.events).OrderBy(e => e.status).ThenBy(e=>e.EE_id).ToList();
+            ViewBag.ChangedEvents = HttpContext.Session.TryGetValue("changed_events", out byte[] value)
+                ? JsonConvert.DeserializeObject<Dictionary<int, bool>>(System.Text.Encoding.UTF8.GetString(value))
+                : new Dictionary<int, bool>();
+            return View(employeeEvents);
+
         }
 
-        public IActionResult ChangeStatus(int ee_id, bool status)
-        {
-            // update status in database
-            var employeeEvent = db.EmployeeEvents.Find(ee_id);
-            employeeEvent.status = status;
-            db.SaveChanges();
 
-            // mark event as changed in session
-            var sessionData = HttpContext.Session.TryGetValue("changed_events", out byte[] value) ? System.Text.Encoding.UTF8.GetString(value) : null;
-            var changed_events = sessionData == null ? new Dictionary<int, bool>() : JsonConvert.DeserializeObject<Dictionary<int, bool>>(sessionData);
-            changed_events[ee_id] = true;
-            HttpContext.Session.SetString("changed_events", JsonConvert.SerializeObject(changed_events));
+
+
+        public IActionResult ChangeStatus(int ee_id, string action)
+        {
+            // get the employee event
+            var employeeEvent = db.EmployeeEvents.Find(ee_id);
+
+            if (action == "confirm")
+            {
+                // update status in database
+                employeeEvent.status = true;
+                db.SaveChanges();
+
+                // mark event as changed in session
+                var changedEvents = GetChangedEvents();
+                changedEvents[ee_id] = true;
+                HttpContext.Session.SetString("changed_events", JsonConvert.SerializeObject(changedEvents));
+            }
+            else if (action == "ignore")
+            {
+                // remove event from database
+                db.EmployeeEvents.Remove(employeeEvent);
+                db.SaveChanges();
+
+                // remove event from session
+                var changedEvents = GetChangedEvents();
+                if (changedEvents.ContainsKey(ee_id))
+                {
+                    changedEvents.Remove(ee_id);
+                    HttpContext.Session.SetString("changed_events", JsonConvert.SerializeObject(changedEvents));
+                }
+            }
+            else
+            {
+                return BadRequest();
+            }
 
             return RedirectToAction("Index");
         }
+
+        private Dictionary<int, bool> GetChangedEvents()
+        {
+            var sessionData = HttpContext.Session.TryGetValue("changed_events", out byte[] value) ? System.Text.Encoding.UTF8.GetString(value) : null;
+            return sessionData == null ? new Dictionary<int, bool>() : JsonConvert.DeserializeObject<Dictionary<int, bool>>(sessionData);
+        }
+
     }
 }
